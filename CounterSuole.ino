6e634c9d76792e2e91d663edd35a5bb1e3c95e14
triloni <42829307+triloni@users.noreply.github.com>
2018-08-30 13:34:45 +0300

    last

--- CounterSuole.ino
+++ CounterSuole.ino
@@ -2,7 +2,7 @@
 
 #define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 40 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define MAX_DISTANCE 30 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 #define LED 10
 #define BUTTON 9
@@ -14,7 +14,7 @@ int delta_cm = 3;
 
 int Error = 0;
 
-int counter = 0;
+int counter = -1;
 
 unsigned long t_refresh_LED = 0;
 
@@ -40,19 +40,12 @@ void setup() {
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int AveradgeDist() {
     float res;
-    Serial.print("Value: ");
     for (int i = 0; i<5; i++) {
-        delay(100);
+        delay(50);
         int k = sonar.ping_cm();
         res = res + k;
-
-        Serial.print(k); Serial.print(" ");
-        
     }
     res = round(res / 5.0);
-
-    Serial.print(" srednee ");
-    Serial.println(res);
     return res;
 }
 
@@ -62,46 +55,51 @@ int AveradgeDist() {
 void loop() {
 
     if (Error == 0) {                                                                                         // если нет ошибки
-        delay(2000);
+        delay(100);
         int t_cm = AveradgeDist();                                                                            // считываем показания датчика
+
+Serial.println(t_cm);
 
         if (t_cm != 0) {                                                                                      // если показание "удачное"
             if (abs(t_cm - Work_Point_cm) > delta_cm) {                                                       // если большое отклонение
-Serial.println("Change value");              
-                Work_Point_cm = t_cm;                                                                         // новая рабочая точка
 
-                counter++;                                                                                    // инкримент счетчика
+                if (Work_Point_cm == -1) {                                                                    // если забрали руку
+                    Work_Point_cm = t_cm;                                                                     // новая рабочая точка
+                    Serial.println("back to good value");
+                } else {                                                                                      // если приклали руку
+                    Work_Point_cm = -1;
 
-                digitalWrite(LED, HIGH);                                                                      // мигаем
-                delay(100);
-                digitalWrite(LED, LOW);
+                    counter++;                                                                                // инкримент счетчика
+
+                    Serial.print("counter++ ");
+Serial.println(counter);
+    
+                    digitalWrite(LED, HIGH);                                                                  // мигаем
+                    delay(100);
+                    digitalWrite(LED, LOW);
+                }
 
                 if (counter == 4 ) {                                                                          // если стопка заполнена
 
-Serial.println("counter = 4");
-                  
                     digitalWrite(LED, HIGH);
                     
                     int dist = 0;                                                                             // временная дистанция
                     do {                                                                                      // цикл ожидания "забирания" стопки стельки
                         delay(100);
-                        dist = AveradgeDist();                                                               // считываем показания датчика
-Serial.println("wait");                        
-                    } while (dist < 25);                                                                      // 
+                        dist = AveradgeDist();                                                                // считываем показания датчика
+                        Serial.print("wait ");                        
+                        Serial.println(dist);
+                    } while (dist < 23);                                                                      // если стопка пустая
 
-Serial.println("wait end");                        
+                    Serial.println("wait end");                        
 
-                    digitalWrite(LED, LOW);
-
-
-                    counter = -1;
-                    Work_Point_cm = AveradgeDist();
-
-                    delay(2000);
+                    digitalWrite(LED, LOW);                                                                   // выключаем светодиод
+                    counter = 0;                                                                              // обнуляем счетчик                    
                     
-                    
+                    delay(4000);                                                                              // пауза для забора стельки
+
                 } else {
-                    delay(2500);                                                                              // пауза для следующей стельки
+                    delay(1500);                                                                              // пауза для следующей стельки
                 }                
             }
         } else {                                                                                              // если ошибка чтения
@@ -112,18 +110,20 @@ Serial.println("wait end");
         int state_LED = 0;
         t_refresh_LED = millis();
 
-        do {                                                                                            // цикл ожидания нажатия на кнопку
+        do {                                                                                                  // цикл ожидания нажатия на кнопку
            if (millis() - t_refresh_LED > 500) {
               state_LED = 1 - state_LED;
               digitalWrite(LED, state_LED);
-              Serial.println("Error");
+//              Serial.println("Error");
               t_refresh_LED = millis();
            }
             
         } while (digitalRead(BUTTON) == LOW);                
-
+        
+        digitalWrite(LED, LOW);                                                                               // выключаем светодиод
+        
         Error = 0;
-        counter = -1;
-        digitalWrite(LED, LOW);
+        Work_Point_cm = AveradgeDist();                                                                       // новая рабочая точка
+        counter = 0;                                                                                          // обнуляем счетчик                    
     }
 }
