907e8a2b0d67a20a5d7532af6f64a9a62cb6573e
Unknown <oleggordiichenko@gmail.com>
2018-09-13 15:33:07 +0300

    IR adaptation

--- IRCounterSuole/IRCounterSuole.ino
+++ IRCounterSuole/IRCounterSuole.ino
@@ -1,23 +1,27 @@
-#include <NewPing.h>
+//#include <NewPing.h>
 
-#define TRIGGER_PIN  12
-#define ECHO_PIN     11
+// #define TRIGGER_PIN  12
+// #define ECHO_PIN     11
 #define MAX_DISTANCE 30
 
 #define LED          10                                                                                       // светодиод
 #define BUTTON        9                                                                                       // кнопка
 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+//NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 float Work_Point_cm = 0.0;
 
-float delta_cm = 0.6;
+float delta_cm = 0.45;
 
 int Error = 0;
 
-int counter = -1;
+int counter = 0; // -1
 
 unsigned long t_refresh_LED = 0;
+
+
+bool flg1 = false; // test IR
+
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // сетап
@@ -29,6 +33,7 @@ void setup() {
     pinMode(BUTTON, INPUT);
 
     Blink();                                                                                                  // мигаем
+
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -55,7 +60,39 @@ float AveradgeDist() {
 //    }
 //    res = round(res / 5.0);
 //    return res;
-    float test_double_accuracy = 0.1*int((sonar.ping_median(20 , MAX_DISTANCE)+0.0000001)/57/0.1);
+
+	//ultrasonic sensor
+    //float test_double_accuracy = 0.1*int((sonar.ping_median(20 , MAX_DISTANCE)+0.0000001)/57/0.1);
+
+
+
+	// IR sensor
+
+	float res = 0;
+    for (int i = 0; i<50; i++) {
+        delay(10);
+        int sensor = 3;
+        int k = analogRead(sensor); //
+        //Serial.println(k);
+        res = res + k;
+    }
+    res = round(res / 50);
+
+	float volts = res*0.0048828125;  // value from sensor * (5/1024)
+	float test_double_accuracy = 13*pow(volts, -1); // worked out from datasheet graph
+  //delay(300); // slow down serial port 
+  
+  // if (distance <= 30){
+  //   //Serial.println(distance);   // print the distance
+
+  //   Serial.println(analogRead(2));
+  // }
+
+	if (test_double_accuracy > 30)
+	{
+		test_double_accuracy = 30;
+	}
+
     return test_double_accuracy;
 }
 
@@ -80,12 +117,43 @@ Serial.println(t_cm);
                 } else {                                                                                      // если приклали руку
                     Work_Point_cm = -1.0;
 
-                    counter++;                                                                                // инкримент счетчика
+                    //counter++;
 
-Serial.print("counter++ ");
-Serial.print(counter);
+
+
+//IR test
+    
+	if (analogRead(1) > 900 && analogRead(2) > 900)
+	{
+		Serial.println("Рука відсутня");
+		flg1 = false;
+	}
+	else if (analogRead(1) < 900 || analogRead(2) < 900)
+	{
+		Serial.println("Рука");
+
+		 while(flg1 == false)
+		{
+			if (analogRead(1) > 900 && analogRead(2) > 900)
+				flg1 = true;
+		}
+
+		counter++;    
+		Serial.print("counter++ ");
+		Serial.print(counter);
+		
+		
+	}
+	// Serial.print("flg1 - ");
+	// Serial.println(flg1);
+
+
+// END                                                                                // инкримент счетчика
+
 Serial.print("  dist ");
 Serial.println(t_cm);
+
+
     
                     digitalWrite(LED, HIGH);                                                                  // мигаем
                     delay(100);
@@ -104,7 +172,7 @@ Serial.println(t_cm);
                         dist = AveradgeDist();                                                                // считываем показания датчика
                         Serial.print("wait ");                        
                         Serial.println(dist);
-                    } while (dist < 24.5);                                                                      // если стопка пустая
+                    } while (dist < 29.5);  // 24.5                                                                    // если стопка пустая
 
 Serial.println("wait end");                        
 
